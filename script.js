a64fc38003febfe82f0cd46d0bf1507f144386d0
Daniyal Saqib <daniyalsaqib04@gmail.com>
2025-09-10 12:05:28 +0500

    Sentence Maker

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+DANIYAL SAQIB
++923064699932
+
+MADE A SENTENCE MAKER
+*/
+
+// as told by freecodecamp to declare variables
+let adjective = "Brave",noun ="Bear",verb = "Angry",place = "Lahore", // I'm a born pakistani why use NY?
+adjective2 ="Calm",noun2 = "Meat";
+
+// string should remain constant all the time
+// ERROR PATA NAHI KYA ARAHA
+const firstStory = "Once upon a time, there was a(n)"
++ " " + adjective + " " +  noun + " " + "who loved to eat"+ " " + noun2 + "." + " " + "The "+ noun + " lived in a " + place + " " + "and had"+ " " + adjective2 + " nostrils that blew fire when it was" + " " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// reassigning everything for new program (new story 2)
+adjective = "mighty";
+
+noun = "dragon";
+
+noun2 = "sheep";
+
+place = "dark cave";
+
+adjective2 = "gigantic";
+
+verb = "angry";
+
+const secondStory = "Once upon a time, there was a(n)"
++ " " + adjective + " " +  noun + " " + "who loved to eat"+ " " + noun2 + ". The "+ noun + " lived in a " + place + " " + "and had"+ " " + adjective2 + " nostrils that blew fire when it was" + " " + verb + ".";
+
+// displaying 2nd story
+console.log("Second story: " + secondStory);
+
