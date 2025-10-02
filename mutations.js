05074b6ae7483c6c4aa39adf3817b72afcdf9191
Daniyal Saqib <daniyalsaqib04@gmail.com>
2025-10-02 11:32:48 +0500

    Finalized version with some help
    This is finalized version i made with Hamza Bhai at WiMetrix

--- mutations.js
+++ mutations.js
@@ -1,65 +1,20 @@
+/*
+QUESTION FROM FREECODECAMP
+I only wrote the function, you can test it by calling it
+*/
+
 function mutation(a) {
-// toLowerCase()
-let ok = true; // boolean value for everything ok
-let daniyal = a[0].toLowerCase();
-let shahzaib = a[1].toLowerCase();
-// everything converted into lower case
-// meaning no stressing out on cases
-// SHAHZAIB is the main concern
+let str1 = a[0].toLowerCase();
+let str2 = a[1].toLowerCase();
 
-// we'll have to sort alphabetically
-
-// converting string to array
-  daniyal = daniyal.split("");
-  shahzaib = shahzaib.split("");
-
-// sorting in ascending order using ASCII
-  daniyal = daniyal.sort();
-  shahzaib = shahzaib.sort();
-
-  /*console.log(daniyal); TESTING DONE
-  console.log(shahzaib);*/
-
- // ARRAY TO STRINGS
-  daniyal = daniyal.join("");
-  shahzaib = shahzaib.join("");
-
-  console.log(daniyal); 
-  console.log(shahzaib);
-
-  if (daniyal === shahzaib) {
-    ok = true;
+for (let i = 0; i < str2.length; i++){
+  if(str1.indexOf(str2[i]) === -1){
+    return false;
   }
-  else if (daniyal.includes(shahzaib)) {
-    ok = true;
-  }
-  else if(shahzaib.includes(daniyal)){
-    ok = true;
-  }
-  else if (daniyal.length > shahzaib.length) {
-    /* count wali logic apply karni hogi */
-    let c = 0; 
-    for (let i = 0; i < daniyal.length; i++) {
-      if (daniyal[i] === shahzaib[i]) {
-        ++c;
-        continue;
-        }
-      else {
-        break;
-      }
-    }
-    if (c === shahzaib.length) {
-      ok = true;
-    }
-    else {
-      ok = false;
-    }
-  }
-  else {
-    ok = false;
-  }
-
-return ok;
 }
 
-console.log(mutation(["floor", "for"]));
+return true;
+}
+
+
+
